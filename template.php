4b6a00eb5d0c2fa24922a0a5bf8627b9dc545d98
setraji <info@setraji.de>
2018-11-27 19:19:55 +0100

    Add files via upload

---
+++ template.php
@@ -0,0 +1,13 @@
+<!doctype html> 
+
+<html>
+	<head>
+		<title>Hallo Welt</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	</head>
+
+	<body>
+		<h1>Startseite</h1>
+		{footer}{content}{navi}
+	</body>
+</html>
